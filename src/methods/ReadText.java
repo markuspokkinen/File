0d6962cc7928a60ff6e63278b92275e1d51e35bd
Markus <Markus@Markus-PC>
2017-06-05 12:11:17 +0300

    text readers

---
+++ src/methods/ReadText.java
@@ -0,0 +1,14 @@
+package methods;
+
+public class ReadText implements ReadTextINF {
+
+	public String read(String file){
+		
+		return null;
+		
+	}
+	public String write(String string) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
---
+++ src/methods/ReadTextINF.java
@@ -0,0 +1,7 @@
+package methods;
+
+public interface ReadTextINF {
+	public String read(String file); //Give a text file that it reads
+	public String write(String string ); //Give a string to save into a file 
+
+}
